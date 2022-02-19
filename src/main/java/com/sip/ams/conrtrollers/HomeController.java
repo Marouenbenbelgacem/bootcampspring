a3a0e53d65b7765f51e127032346630780c98dea
Marwen <bmarouen@outlook.com>
2022-02-19 21:50:30 +0100

    adding CSS and Images

--- src/main/java/com/sip/ams/conrtrollers/HomeController.java
+++ src/main/java/com/sip/ams/conrtrollers/HomeController.java
@@ -1,5 +1,8 @@
 package com.sip.ams.conrtrollers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +13,16 @@ public class HomeController {
 	@GetMapping ("/info")
 	public String info (Model model) {
 		
+		List<String> names = new ArrayList<String>();
+		names.add("OCA");
+		names.add("OCP");
+		names.add("Spring");
+		
 		String formation = "FullStack";
 		System.out.println("Bonjour methode info");
 		
 		model.addAttribute("workshop", formation);
+		model.addAttribute("names", names);
 		return "home/info";
 	}
 	@GetMapping ("/affichage")

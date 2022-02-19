f5823b8ff7a8592bad0687f2a0a850a371966773
Marwen <bmarouen@outlook.com>
2022-02-19 23:49:27 +0100

    fin add forms

--- src/main/java/com/sip/ams/conrtrollers/HomeController.java
+++ src/main/java/com/sip/ams/conrtrollers/HomeController.java
@@ -6,10 +6,19 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+	@PostMapping("/result")
+	@ResponseBody
+	public String resultat(@RequestParam("nom") String n , @RequestParam("email") String e) {
+		return "voua avez taper :"+ n + " " + e ;
+	}
+	
 	@GetMapping ("/info")
 	public String info (Model model) {
 		

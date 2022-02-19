727988b0da58d268edbd0e70dfce52ac9d670c7c
Marwen <bmarouen@outlook.com>
2022-02-19 17:33:39 +0100

    my third commit

--- src/main/java/com/sip/ams/conrtrollers/HomeController.java
+++ src/main/java/com/sip/ams/conrtrollers/HomeController.java
@@ -1,15 +1,19 @@
 package com.sip.ams.conrtrollers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping ("/info")
-	public String info () {
+	public String info (Model model) {
+		
+		String formation = "FullStack";
 		System.out.println("Bonjour methode info");
+		
+		model.addAttribute("workshop", formation);
 		return "home/info";
 	}
 	@GetMapping ("/affichage")
